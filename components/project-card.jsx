57b662589e43c9ba75ceea37075623b1d1f2d409
Robert Woods <robertjawoods@gmail.com>
2021-06-26 20:14:57 +0100

    Changed project cards

--- components/project-card.jsx
+++ components/project-card.jsx
@@ -40,12 +40,17 @@ function ProjectCard(props) {
 
     return (
         <> {repoData ? (
-            <div className="bg-blue rounded-xl p24 md:p-0 w-60 py-70">
-                <div className="text-center text-peach">
-                    <a href={repoData.url}>
-                        <h2 className="underline">{repoData.name}</h2>
-                    </a>
-                    <p>{repoData.description}</p>
+            <div class="rounded overflow-hidden shadow-2xl">
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">{repoData.name}</div>
+                    <p class="text-gray-700 text-base">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                </div>
+                <div class="px-6 pt-4 pb-2">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#fall</span>
                 </div>
             </div>
         ) : ""}
--- pages/projects.jsx
+++ pages/projects.jsx
@@ -6,11 +6,10 @@ export default function Projects() {
         <>
             <Navbar />
 
-            <div>
-                <h1 className="font-raleway">Projects!</h1>
-
+            <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
                 <ProjectCard repo="LoLShamer" owner="robertjawoods" />
                 <ProjectCard repo="LudumDare48" owner="Blindside-Games" />
+                <ProjectCard repo="AutoHideObjectives" owner="robertjawoods" />
                 <ProjectCard repo="AutoHideObjectives" owner="robertjawoods" />
             </div>
         </>
