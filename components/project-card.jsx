439de93cb7611a65722f4e4bf8527327fe19ec18
Robert Woods <robertjawoods@gmail.com>
2021-06-26 20:27:40 +0100

    added tags to cards

--- components/project-card.jsx
+++ components/project-card.jsx
@@ -44,13 +44,15 @@ function ProjectCard(props) {
                 <div class="px-6 py-4">
                     <div class="font-bold text-xl mb-2">{repoData.name}</div>
                     <p class="text-gray-700 text-base">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                        {repoData.description}
                     </p>
                 </div>
                 <div class="px-6 pt-4 pb-2">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#fall</span>
+                    {
+                        (props.tags || []).map(tag =>
+                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{tag}</span>
+                        )
+                    }
                 </div>
             </div>
         ) : ""}
--- pages/projects.jsx
+++ pages/projects.jsx
@@ -7,9 +7,9 @@ export default function Projects() {
             <Navbar />
 
             <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-                <ProjectCard repo="LoLShamer" owner="robertjawoods" />
-                <ProjectCard repo="LudumDare48" owner="Blindside-Games" />
-                <ProjectCard repo="AutoHideObjectives" owner="robertjawoods" />
+                <ProjectCard repo="LoLShamer" owner="robertjawoods" tags={["javascript", "discord", "googlecloudplatform"]} />
+                <ProjectCard repo="LudumDare48" owner="Blindside-Games" tags={["c#", "gamedev", "unity"]} />
+                <ProjectCard repo="AutoHideObjectives" owner="robertjawoods" tags={["lua"]} />
                 <ProjectCard repo="AutoHideObjectives" owner="robertjawoods" />
             </div>
         </>
